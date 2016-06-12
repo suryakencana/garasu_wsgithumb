399389d4809d417d3a27f7949e997a620d203999
Surya 'Zi <worksuryakencana@gmail.com>
2016-06-13 04:08:40 +0700

    garasu_wsgithumb landing on

---
+++ README.txt
@@ -0,0 +1,1 @@
+garasu_wsgithumb README
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'Pillow'
+    ]
+
+setup(name='garasu_wsgithumb',
+      version='0.1',
+      description='garasu_wsgithumb',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web pyramid garasu_wsgithumb',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="garasu_wsgithumb",
+      entry_points="""\
+      """,
+      )
